1d2b8ce5446d1bbf7a48ae1388fb604d03cbc231
dfd <fasfa>
2020-05-07 21:22:41 +0200

    + container add

--- src/App.js
+++ src/App.js
@@ -14,25 +14,162 @@ function App() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
                         </li>
                     </ul>
 
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
-                        <a className="nav-link" href="#">Search <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="#">Search<span className="sr-only">(current)</span></a>
                         </li>
                     </ul>
-                    
                 </div>
             </nav>
 
             <h1 className="apptitle">APP TITLE</h1>
 
-            
+            <div className="container num1">
+                <div className="row">
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Two
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Three
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                </div>
+            </div>
+
+            <div className="container num2">
+                <div className="row">
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Two
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Three
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                </div>
+            </div>
+
+            <div className="container num3">
+                <div className="row">
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Two
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Three
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                </div>
+            </div>
+
+            <div className="container num4">
+                <div className="row">
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Two
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Three
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                </div>
+            </div>
+
+            <div className="container num5">
+                <div className="row">
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Two
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Three
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                </div>
+            </div>
+
+            <div className="container num6">
+                <div className="row">
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Two
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        Three
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                    <div className="col-12 col-sm-4 col-md-2">
+                        One
+                    </div>
+                </div>
+            </div>
+
+
+
+
         </div> /*MainDiv*/
-
-
 
         
     )
