738fce88a803dc0c3ab1bebc04a97556795ae191
dfd <fasfa>
2020-05-07 20:51:18 +0200

    / indentation and active remove

--- src/App.js
+++ src/App.js
@@ -4,38 +4,33 @@ import './App.scss';
 
 function App() {
     return (
-<div id="main">
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">ZdendoTestApp</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-        </button>
+        <div id="main">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">ZdendoTestApp</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-            </ul>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
 
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item active">
-                <a className="nav-link" href="#">Search <span className="sr-only">(current)</span></a>
-                </li>
-            </ul>
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Search <span className="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                    
+                </div>
+            </nav>
+
+            <h1 className="apptitle">APP TITLE</h1>
+
             
-        </div>
-    </nav>
-
-    <h1 className="apptitle">APP TITLE</h1>
-
-    
-          
-
-
-
-
- </div> /*MainDiv*/
+        </div> /*MainDiv*/
 
 
 
